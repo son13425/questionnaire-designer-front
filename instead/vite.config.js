be8a798cab5c317f76e2ea5a478cd214add5a46a
Оксана Широкова <son134@yandex.ru>
2024-07-04 12:40:59 +0300

    Многострааничник

--- instead/vite.config.js
+++ instead/vite.config.js
@@ -1,7 +1,16 @@
 import Inspect from 'vite-plugin-inspect'
+import { resolve } from 'path'
 
 export default {
     plugins: [
         Inspect()
     ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                login: resolve(__dirname, 'src/form/index.html')
+            }
+        }
+    }
 }
