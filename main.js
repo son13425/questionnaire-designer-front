af691023689e1a92a00925fba74fc0ce847bb092
Оксана Широкова <son134@yandex.ru>
2024-07-04 21:10:38 +0300

    16

--- main.js
+++ main.js
@@ -3,5 +3,3 @@ import './style.css'
 // Импорт картинок
 import centralIcon from './src/images/logo-login.png'
 document.querySelector('#central-icon').src = centralIcon
-import urlForm from './src/form/index.html?url'
-document.querySelector('#url-form').href = urlForm
