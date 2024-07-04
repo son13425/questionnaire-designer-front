b848dc9c0491315b7e762b1e22739a817e166c4e
Оксана Широкова <son134@yandex.ru>
2024-07-04 17:21:42 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import Inspect from 'vite-plugin-inspect'
 import { resolve } from 'path'
 
 
 export default {
-    base: '/questionnaire-designer-front/',
-    plugins: [
-        Inspect()
-    ],
     build: {
-        outDir: '../dist',
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
