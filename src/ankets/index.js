d5c7ed78d9fc3cea26c15fd3545756f06814a94e
Elena Shirokova <stolar3@yandex.ru>
2024-07-07 01:49:07 +0300

    ankets

---
+++ src/ankets/index.js
@@ -0,0 +1,19 @@
+// Импорт стилей
+import '/src/index.css';
+import '/src/ankets/index.css';
+
+// Импорт картинок
+import footerLogo from '/src/images/ias-logo-3.png';
+import headerLogo from '/src/images/logo-med.png';
+import menuIcon from '/src/images/menu-icon.svg';
+import userAvatar from '/src/images/user-icon.svg';
+import backIcon from '/src/images/left-icon.svg';
+import listIcon from '/src/images/list-icon.svg';
+document.querySelector('#footer-logo').src = footerLogo;
+document.querySelector('#header-logo').src = headerLogo;
+document.querySelector('#menu-icon').src = menuIcon;
+document.querySelector('#avatar').src = userAvatar;
+document.querySelector('#back1').src = backIcon;
+document.querySelector('#list-icon1').src = listIcon;
+document.querySelector('#back2').src = backIcon;
+document.querySelector('#list-icon2').src = listIcon;
